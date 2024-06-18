89f58680be3c383d3e9f16129831fcc17e100019
SimoneLaRocca <83709227+SimoneLaRocca@users.noreply.github.com>
2024-06-19 01:07:51 +0200

    Add files via upload

---
+++ smart-contracts/EthereumDIDRegistry.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.6;
+
+contract EthereumDIDRegistry {
+
+    mapping(string => bytes) public didDocuments;
+
+    event DIDDocumentUpdated(
+        string did,
+        bytes document
+    );
+
+    function updateDIDDocument(string memory _did, bytes memory _document) public {
+        require(bytes(_did).length > 0, "Invalid DID");
+        require(_document.length > 0, "Empty document");
+
+        didDocuments[_did] = _document;
+
+        emit DIDDocumentUpdated(_did, _document);
+    }
+
+    function getDIDDocument(string memory _did) public view returns (bytes memory document) {
+        return didDocuments[_did];
+    }
+}
